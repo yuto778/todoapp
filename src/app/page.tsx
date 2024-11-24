0f73e2137607d1f49eadcbaad95868f089d986b2
yuto778 <yuto4949@i.softbank.jp>
2024-11-24 14:18:05 +0900

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { redirect } from "next/navigation";
-import { createClient } from "../../lib/client";
-import Topbar from "@/components/Topbar";
 import Sidebar from "@/components/Sidebar";
+import Topbar from "@/components/Topbar";
 
 export default async function Home() {
-  const supabase = createClient();
-
-  const { data, error } = await supabase.from("test").select("*").single();
-
-  if (error) {
-    redirect("/");
-  }
   return (
     <>
       <div className="h-screen w-screen bg-yellow-200/40 flex flex-col ">
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import { createClient } from "../../lib/client";
-
 const Sidebar = async () => {
-  const supabase = createClient();
-  const { data } = await supabase.from("test").select("name").single();
   return (
     <div className="w-40 bg-gray-100 h-full flex flex-col items-center lg:hidden py-5 ">
       <h2 className="font-bold text-2xl">Sidebar</h2>
-      <h2>{data?.name}</h2>
+      <h2>yuto778</h2>
     </div>
   );
 };
