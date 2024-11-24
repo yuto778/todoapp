ec3b5b6877db6b51eff060cd28fa28ccb31e9ce8
yuto778 <yuto4949@i.softbank.jp>
2024-11-24 14:36:42 +0900

    test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+import Topbar from "@/components/Topbar";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Topbar />
+      <Sidebar />
+    </>
+  );
+};
+
+export default page;
