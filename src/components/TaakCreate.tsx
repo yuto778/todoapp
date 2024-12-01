fbc1d961c9a4c55ebd1d665fbcb9ce07636a09e5
yuto778 <yuto4949@i.softbank.jp>
2024-12-01 10:29:22 +0900

    fix

--- src/components/TaakCreate.tsx
+++ src/components/TaakCreate.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import React from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
 import BackPage from "./BackPage";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
